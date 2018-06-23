1d4a01462fa2a5e7dd33af5619e74f233990231f
Yesh Chandiramani <yesh0907@hotmail.com>
2018-06-23 12:41:43 +0800

    create items

---
+++ items/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=255)
+    image_b64_addr = models.TextField()
+    description = models.TextField()
+    seller = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    best_before_date = models.DateField(auto_now_add=False, auto_now=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)

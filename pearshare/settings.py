451fa64dcb59195f72772cac74f571e357a443de
Yesh Chandiramani <yesh0907@hotmail.com>
2018-06-23 12:07:11 +0800

    update db settings

--- pearshare/settings.py
+++ pearshare/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,7 +73,12 @@ WSGI_APPLICATION = 'pearshare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = { 'default': dj_database_url.config() }
+DATABASES = { 
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    } 
+}
 
 
 # Password validation

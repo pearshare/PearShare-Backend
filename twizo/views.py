ab2523b4ed9cee52c2df55ceafadf4b7f669f7f4
Yesh Chandiramani <yesh0907@hotmail.com>
2018-06-23 18:47:00 +0800

    pseudocode for twizo

--- twizo/views.py
+++ twizo/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 TWIZO_TEST_API_KEY = 'OVSXDvByaLF9GxXebEUHelIxY7IemJwRyn6iPzEqZA5SRZvI'
+
+def request_payment_and_send_conf():
+    find user's phone number # this is just hardcoded 98553351
+    make req to twizo api
+    wait for it to send
+    store messageId into db and attach to users. find user by username given in the request
+
+def confirm_token
+    use user token entered and messageId to request on twizo api
+    if it is right
+        return success!
+    else
+        return wrong token 

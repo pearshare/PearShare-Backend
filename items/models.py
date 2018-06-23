5cfe0a8f7db82ab56d4b36035c9b5fedfb9ad6e5
Yesh Chandiramani <yesh0907@hotmail.com>
2018-06-23 13:03:37 +0800

    add to item to admin dashboard

--- items/models.py
+++ items/models.py
@@ -15,3 +15,6 @@ class Item(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
